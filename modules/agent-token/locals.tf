a024611690414c71bf41f1269bf92c2e858f0b26
partogisalomon <partogi.salomon@traveloka.com>
2019-08-28 12:03:11 +0700

    add agent token output

--- modules/agent-token/locals.tf
+++ modules/agent-token/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  eciisa_token_name = "/tvlk-secret/shared/eci/eciisa/agent.token"
+  eciisa_token_name = "/tvlk-secret/shared/eci/eciisa/eciisa-app/eciisa-app-token/agent_token.key"
   eciisa_token_desc = "Informatica Secure Agent token"
   product_domain    = "eci"
   environment       = "${var.environment}"
---
+++ modules/agent-token/outputs.tf
@@ -0,0 +1,4 @@
+output "token_name" {
+  description = "Informatica Secure Agent token name"
+  value       = "${local.eciisa_token_name}"
+}
